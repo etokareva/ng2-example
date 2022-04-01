1f8458cce489dcc6fa0f1b1ef37c0496fc239260
Ekaterina <ekatert@labcorp.com>
2022-04-01 17:42:43 +0300

    example/define-column-async/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+import { GridModel, Grid } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = [
+	'define-column-async',
+	'Table content is loaded asynchronously'
+];
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent implements OnInit {
+	static tags = EXAMPLE_TAGS;
+	title = EXAMPLE_TAGS[1];
+
+	gridModel: GridModel;
+
+	constructor(private dataService: DataService, qgrid: Grid) {
+		this.gridModel = qgrid.model();
+	}
+
+	ngOnInit() {
+		this.dataService
+			.getAtoms()
+			.subscribe(rows => {
+				this.gridModel.data({ rows });
+
+				setTimeout(() => {
+					this.gridModel.data({
+						columns: [
+							{
+								key: 'source',
+								width: 300
+							},
+							{
+								key: 'symbol+name',
+								label: row => `[${row.symbol}]${row.name}`,
+								value: row => row.symbol,
+								width: 150
+							}
+						]
+					});
+				}, 1000);
+			});
+	}
+}
