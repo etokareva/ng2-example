2766af4646a1f2bce000aa92e7987e929710461c
Ekaterina <ekatert@labcorp.com>
2022-04-01 18:32:38 +0300

    example/pane-with-selection-mode/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, ChangeDetectionStrategy, AfterViewInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Grid, GridModel, PaneComponent } from 'ng2-qgrid';
+import { DataService, Atom } from '../data.service';
+
+const EXAMPLE_TAGS = [
+	'pane-with-selection',
+	'Pane for each row can be opened by clicking on the corresponding row. Also you can select rows on left selection column'
+];
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExamplePaneWithSelectionComponent implements AfterViewInit {
+
+	constructor(dataService: DataService,
+		private qgrid: Grid
+	) {
+		this.rows$ = dataService.getAtoms();
+		this.gridModel = qgrid.model();
+	}
+
+	static tags = EXAMPLE_TAGS;
+
+	@ViewChild(PaneComponent) pane: PaneComponent;
+
+	title = EXAMPLE_TAGS[1];
+
+	rows$: Observable<Atom[]>;
+	gridModel: GridModel;
+
+	ngAfterViewInit(): void {
+		this.gridModel.mouseChanged.on(({ state }) => {
+			const { code, status, target } = state;
+			if (code === 'left' && status === 'down') {
+				if (target && target.column.type !== 'select') {
+					this.pane.open('right', target.row);
+				}
+			}
+		});
+	}
+}
