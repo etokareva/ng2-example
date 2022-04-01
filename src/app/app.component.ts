89834c8912e8b3f15b7027a12e9a92781b4389d9
Ekaterina <ekatert@labcorp.com>
2022-04-01 18:33:57 +0300

    example/rest-api-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+
+const EXAMPLE_TAGS = [
+	'rest-api-basic',
+	'REST-plugin demonstration. All actions are performed on server side'
+];
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static tags = EXAMPLE_TAGS;
+	title = EXAMPLE_TAGS[1];
+}
