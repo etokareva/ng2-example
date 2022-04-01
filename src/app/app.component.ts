feb27c6a26a12b0a06cb02603e53f9ba295e63f4
Ekaterina <ekatert@labcorp.com>
2022-04-01 17:41:09 +0300

    example/column-row-indicator-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+const EXAMPLE_TAGS = [
+	'column-row-indicator-basic',
+	'Table contains row indicators'
+];
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static tags = EXAMPLE_TAGS;
+	title = EXAMPLE_TAGS[1];
+
+	rows = [
+		{},
+		{},
+		{},
+		{}
+	];
+}
